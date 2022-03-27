6dd9117547084db66c2ebbfecb5b1098f9edfa8e
VictoryLinux <63359543+VictoryLinux@users.noreply.github.com>
2022-03-27 14:57:22 -0500

    Update victory.sh

--- victory.sh
+++ victory.sh
@@ -75,16 +75,10 @@ echo -e "----------------------------------------------------------------"
 echo -e " DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK. "
 echo -e "----------------------------------------------------------------"
 
-echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-echo "++++++++  Things you need to know before you start  ++++++++"
-echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-echo "++                                                        ++"
-echo "++ 1.) "This is NOT a silent install"                      ++"
-echo "++                                                        ++"
-echo "++ "you will be asked several questions as it progresses"  ++"
-echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-	
+echo -e "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo -e "++++++++         This is NOT a silent install        ++++++++"
+echo -e "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+
 	
 #	sleep 5s
 	echo "ARE YOU READY TO START? [y,n]"
@@ -306,25 +300,25 @@ done
 	echo
 	sudo dnf install brave-browser -y
 	echo
-    sudo rpm --import https://packagecloud.io/shiftkey/desktop/gpgkey
-    echo
-    sudo sh -c 'echo -e "[shiftkey]\nname=GitHub Desktop\nbaseurl=https://packagecloud.io/shiftkey/desktop/el/7/\$basearch\nenabled=1\ngpgcheck=0\nrepo_gpgcheck=1\ngpgkey=https://packagecloud.io/shiftkey/desktop/gpgkey" > /etc/yum.repos.d/shiftkey-desktop.repo' 
-    echo
-    sudo dnf install github-desktop
-    echo
-    sudo dnf copr enable elxreno/multimc -y && sudo dnf install multimc -y
-    echo
+    	sudo rpm --import https://packagecloud.io/shiftkey/desktop/gpgkey
+    	echo
+    	sudo sh -c 'echo -e "[shiftkey]\nname=GitHub Desktop\nbaseurl=https://packagecloud.io/shiftkey/desktop/el/7/\$basearch\nenabled=1\ngpgcheck=0\nrepo_gpgcheck=1\ngpgkey=https://packagecloud.io/shiftkey/desktop/gpgkey" > /etc/yum.repos.d/shiftkey-desktop.repo' 
+    	echo
+    	sudo dnf install github-desktop
+    	echo
+    	sudo dnf copr enable elxreno/multimc -y && sudo dnf install multimc -y
+    	echo
 	sleep 3s
 
 	flatpak install flathub com.discordapp.Discord -y
 	flatpak install flathub org.onlyoffice.desktopeditors -y
 	flatpak install flathub com.simplenote.Simplenote -y
 	flatpak install flathub com.system76.Popsicle -y
-    flatpak install flathub com.vscodium.codium -y
-    flatpak install flathub com.mattjakeman.ExtensionManager -y
-    flatpak install flathub com.bitwarden.desktop -y
-    flatpak install flathub nl.hjdskes.gcolor3 -y
-    flatpak install flathub com.usebottles.bottles -y
+    	flatpak install flathub com.vscodium.codium -y
+    	flatpak install flathub com.mattjakeman.ExtensionManager -y
+    	flatpak install flathub com.bitwarden.desktop -y
+    	flatpak install flathub nl.hjdskes.gcolor3 -y
+    	flatpak install flathub com.usebottles.bottles -y
 
 
 
@@ -332,7 +326,7 @@ done
 	flatpak remote-add --if-not-exists plex-media-player https://flatpak.knapsu.eu/plex-media-player.flatpakrepo
 	flatpak install plex-media-player tv.plex.PlexMediaPlayer -y
 	echo
-    cd ~/
+    	cd ~/
 	git clone https://github.com/ryanoasis/nerd-fonts
 	cd ~/nerd-fonts
 	./install.sh FiraCode
@@ -349,7 +343,7 @@ function backgrounds() {
 	echo
 	sleep 6s
 	cd ~/
-    git clone https://gitlab.com/dwt1/wallpapers.git
+    	git clone https://gitlab.com/dwt1/wallpapers.git
 
 	check_exit_status
 }
@@ -362,54 +356,54 @@ function configs() {
 	echo "##################################"
 	echo
 	sleep 6s
-    echo
-    export PATH=$PATH:~/.local/bin
-    cp -r $HOME/VictoryFedora/configs/* $HOME/.config/
-    echo
-    mv $HOME/.config/bashrc $HOME/.config/.bashrc
-    mv $HOME/.config/.bashrc $HOME
-    echo
-    mv $HOME/.config/face $HOME/.config/.face
-    mv $HOME/.config/.face $HOME
+   	echo
+    	export PATH=$PATH:~/.local/bin
+    	cp -r $HOME/VictoryFedora/configs/* $HOME/.config/
+    	echo
+    	mv $HOME/.config/bashrc $HOME/.config/.bashrc
+    	mv $HOME/.config/.bashrc $HOME
+    	echo
+    	mv $HOME/.config/face $HOME/.config/.face
+    	mv $HOME/.config/.face $HOME
     
 }
 
 function appearance() {
-cd $HOME/VictoryFedora/
-git clone https://github.com/daniruiz/flat-remix
-git clone https://github.com/daniruiz/flat-remix-gtk
-#mkdir -p ~/.icons && mkdir -p ~/.themes
-#cp -r flat-remix/Flat-Remix* ~/.icons/ && cp -r flat-remix-gtk/themes/Flat-Remix* ~/.themes/
-sudo mv flat-remix/Flat-Remix* /usr/share/icons/ 
-sudo mv flat-remix-gtk/themes/Flat-Remix* /usr/share/themes/
-rm -rf ~/flat-remix flat-remix-gtk
-gsettings set org.gnome.desktop.interface gtk-theme "Flat-Remix-GTK-Blue-Dark"
-gsettings set org.gnome.desktop.interface icon-theme "Flat-Remix-Blue-Dark"
-echo
-gsettings set org.gnome.shell favorite-apps "['brave-browser.desktop', 'firefox.desktop', 'org.gnome.Nautilus.desktop', 'terminator.desktop', 'com.simplenote.Simplenote.desktop', 'virtualbox.desktop', 'com.vscodium.codium.desktop', 'onboard.desktop']"
-gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,maximize,close"
-gsettings set org.gnome.desktop.interface clock-format '12h'   
-gsettings set org.gnome.desktop.interface cursor-theme 'Breeze'
+	cd $HOME/VictoryFedora/
+	git clone https://github.com/daniruiz/flat-remix
+	git clone https://github.com/daniruiz/flat-remix-gtk
+	#mkdir -p ~/.icons && mkdir -p ~/.themes
+#	cp -r flat-remix/Flat-Remix* ~/.icons/ && cp -r flat-remix-gtk/themes/Flat-Remix* ~/.themes/
+	sudo mv flat-remix/Flat-Remix* /usr/share/icons/ 
+	sudo mv flat-remix-gtk/themes/Flat-Remix* /usr/share/themes/
+	rm -rf ~/flat-remix flat-remix-gtk
+	gsettings set org.gnome.desktop.interface gtk-theme "Flat-Remix-GTK-Blue-Dark"
+	gsettings set org.gnome.desktop.interface icon-theme "Flat-Remix-Blue-Dark"
+	echo
+	gsettings set org.gnome.shell favorite-apps "['brave-browser.desktop', 'firefox.desktop', 'org.gnome.Nautilus.desktop', 'terminator.desktop', 'com.simplenote.Simplenote.desktop', 'virtualbox.desktop', 'com.vscodium.codium.desktop', 'onboard.desktop']"
+	gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,maximize,close"
+	gsettings set org.gnome.desktop.interface clock-format '12h'   
+	gsettings set org.gnome.desktop.interface cursor-theme 'Breeze'
 
-check_exit_status
+	check_exit_status
 }
 
 #EXTENSIONS
 function extensions() {
-gnome-extensions enable caffeine@patapon.info
-gnome-extensions enable dash-to-dock@micxgx.gmail.com
-#gnome-extensions enable impatience@gfxmonk.net
-#gnome-extensions enable noannoyance@daase.net
-gnome-extensions enable pop-shell@system76.com
-#gnome-extensions enable tiling-assistant@leleat-on-github
-#gnome-extensions enable Vitals@CoreCoding.com
-gnome-extensions enable window-list@gnome-shell-extensions.gcampax.github.com
-gnome-extensions enable sound-output-device-chooser@kgshank.net
-#gnome-extensions enable gnome-ui-tune@itstime.tech
-#gnome-extensions enable pamac-updates@manjaro.org
-#gnome-extensions enable taskicons@rliang.github.com
+	gnome-extensions enable caffeine@patapon.info
+	gnome-extensions enable dash-to-dock@micxgx.gmail.com
+#	gnome-extensions enable impatience@gfxmonk.net
+#	gnome-extensions enable noannoyance@daase.net
+	gnome-extensions enable pop-shell@system76.com
+#	gnome-extensions enable tiling-assistant@leleat-on-github
+#	gnome-extensions enable Vitals@CoreCoding.com
+	gnome-extensions enable window-list@gnome-shell-extensions.gcampax.github.com
+	gnome-extensions enable sound-output-device-chooser@kgshank.net
+#	gnome-extensions enable gnome-ui-tune@itstime.tech
+#	gnome-extensions enable pamac-updates@manjaro.org
+#	gnome-extensions enable taskicons@rliang.github.com
 
-check_exit_status
+	check_exit_status
 }
 
 #GPU
@@ -448,11 +442,11 @@ fi
 
 #GRUB
 function grub() {
-cd $HOME/VictoryFedora/grub/
-sudo chmod +x install.sh
-sudo ./install.sh
+	cd $HOME/VictoryFedora/grub/
+	sudo chmod +x install.sh
+	sudo ./install.sh
 
-check_exit_status
+	check_exit_status
 }
 
 #
@@ -468,7 +462,7 @@ function clean-up() {
 	sudo rm -rf /usr/share/backgrounds/workstation
 	echo
 	sudo rm -rf ~/nerd-fonts
-    echo
+    	echo
 	check_exit_status
 }
 
